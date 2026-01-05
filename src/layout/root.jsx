4d4288e8c99a57769c35df7c02879f40d8f021f9
codersrabbi <pcrabbi2020@gmail.com>
2026-01-05 18:46:54 +0600

    navbar almsot done

--- src/layout/root.jsx
+++ src/layout/root.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Footer from '../shared/footer';
+import Navbar from '../shared/navbar';
 
 const root = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet>
 
             </Outlet>
--- src/shared/navbar.jsx
+++ src/shared/navbar.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router';
 
-const navbar = () => {
+const Navbar = () => {
     return (
-        <div>
-            <h1>This is navbar</h1>
+        <div className="navbar absolute  shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+
+                    {/* // Submenu */}
+                    {/* <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a>Item 1</a></li>
+                        <li>
+                            <a>Parent</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul> */}
+                </div>
+                <a className="btn btn-ghost text-xl text-white">MN Tech</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+
+                {/* Sunmenu */}
+                <ul className="flex gap-3 text-xl font-semibold text-white">
+                    <Link to="/">Home</Link>
+                    <Link to="#">Post Style</Link>
+                    <Link to="#">Contact</Link>
+                </ul>
+            </div>
         </div>
     );
 };
 
-export default navbar;
+export default Navbar;
