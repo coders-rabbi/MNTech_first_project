8ff247614d79e54ae335e1a300a9d4846b9fcdec
codersrabbi <pcrabbi2020@gmail.com>
2026-01-05 14:19:58 +0600

    Project setup done

---
+++ src/layout/root.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router';
+
+const root = () => {
+    return (
+        <div>
+            <Outlet>
+                
+            </Outlet>
+        </div>
+    );
+};
+
+export default root;
---
+++ src/router/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom"
+import root from "../layout/root";
+import { Component } from "react";
+import Home from "../pages/Home/home";
+// import { RouterProvider } from "react-router/dom";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: root,
+        children: [
+            {
+                path: "/",
+                Component: <Home />
+            }
+        ],
+    },
+]);
